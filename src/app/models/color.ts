da482bade0cdbca2abf439217931c9e53867a210
Santdev404 <santdev404@gmail.com>
2021-06-04 08:57:28 -0500

    Adding User & Color models

---
+++ src/app/models/color.ts
@@ -0,0 +1,10 @@
+export class Color{
+    constructor(
+        public id: number,
+        public name: string,
+        public color: string,
+        public pantone: string,
+        public year: any,
+        public createdAt: any
+    ){}
+}
